f458b005524a48fa13c98556a2bf0f55b4d5cfce
ochiaikouta <otiaikouta1118@gmail.com>
2025-09-28 20:51:14 +0900

    keyマップのtabの割り当てを変更

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -19,19 +19,47 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
-        mapping = cmp.mapping.preset.insert({
+        window = {
+          completion = cmp.config.window.bordered(), -- 候補リストに枠
+          documentation = cmp.config.window.bordered({
+            max_height = 15, -- ドキュメントウィンドウの最大行数
+            max_width = 80,  -- 最大幅
+          }),
+        },
+        mapping = {
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
+
+          -- Tab / Shift-Tab を補完リストとスニペット用に設定
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
         }, {
           { name = "buffer" },
-        })
+        }),
       })
     end,
   },
